61527de733c22cb5eb941914b03508371b1fc0c4
Peter Polacik <peter@polacik.org>
2025-01-19 16:41:08 +0100

    emacs: Load quicklisp slime helper on Mac OS X too

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -314,9 +314,13 @@ If the new path's directories do not exist, create them."
 
 (pp/fix-utf)
 
-(setq ql-slime-helper (expand-file-name "~/.quicklisp/slime-helper.el"))
-(when (file-exists-p ql-slime-helper)
-  (load ql-slime-helper))
+(setq ql-slime-helpers (list (expand-file-name "~/.quicklisp/slime-helper.el")
+                             (expand-file-name "~/quicklisp/slime-helper.el")))
+(catch 'slime-helper-load
+  (dolist (helper-candidate ql-slime-helpers)
+    (when (file-exists-p helper-candidate)
+      (throw 'slime-helper-load
+        (load helper-candidate)))))
 (setq inferior-lisp-program (executable-find "sbcl"))
 
 (setq tab-bar-format
