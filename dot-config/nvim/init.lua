cf23458ee13b2466bead37ac391db7aeb4952796
Peter Polacik <peter@polacik.org>
2025-05-29 00:26:19 +0200

    neovim: add initial cinfiguration

---
+++ dot-config/nvim/init.lua
@@ -0,0 +1,56 @@
+-- Setup leader keys
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any ley to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup plugins
+require("lazy").setup({
+  spec = {
+    { "miikanissi/modus-themes.nvim", priority = 1000 },
+    { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" },
+    { "neovim/nvim-lspconfig" },
+  },
+  install = { colorscheme = { "modus" } },
+  checker = { enabled = true },
+})
+
+-- LSP
+vim.lsp.enable("gopls")
+
+-- Keybindings
+-- Normal mode
+vim.keymap.set("n", "<leader>n", ":set invnumber<CR>")
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+-- Insert mode
+vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
+-- Visual mode
+
+-- Theme
+vim.cmd([[colorscheme modus]])
+
+-- Setup
+vim.opt.nu = true
