60bcda0397fc483b74f0ef5b7ef19d381871aef8
Peter Polacik <peter@polacik.org>
2025-05-29 10:45:59 +0200

    neovim: add gitsigns plugin and set et

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -22,7 +22,14 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup plugins
 require("lazy").setup({
   spec = {
-    { "miikanissi/modus-themes.nvim", priority = 1000 },
+    {
+      "miikanissi/modus-themes.nvim",
+      priority = 1000,
+      lazy = false,
+      config = function()
+        vim.cmd([[colorscheme modus]])
+      end,
+    },
     { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
     { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" },
     { "neovim/nvim-lspconfig" },
@@ -44,7 +51,77 @@ require("lazy").setup({
         },
       },
     },
-    { "nvim-tree/nvim-web-devicons" },
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+    { url = "https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git" },
+    {
+      "lewis6991/gitsigns.nvim",
+      opts = {
+        on_attach = function(bufnr)
+          local gitsigns = require("gitsigns")
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map("n", "]c", function()
+            if vim.wo.diff then
+              vim.cmd.normal({"]c", bang = true})
+            else
+              gitsigns.nav_hunk("next")
+            end
+          end)
+
+          map("n", "[c", function()
+            if vim.wo.diff then
+              vim.cmd.normal({"[c", bang = true})
+            else
+              gitsigns.nav_hunk("prev")
+            end
+          end)
+
+          -- Actions
+          map("n", "<leader>hs", gitsigns.stage_hunk)
+          map("n", "<leader>hr", gitsigns.reset_hunk)
+
+          map("v", "<leader>hs", function()
+            gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end)
+
+          map("v", "<leader>hr", function()
+            gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end)
+
+          map("n", "<leader>hS", gitsigns.stage_buffer)
+          map("n", "<leader>hR", gitsigns.reset_buffer)
+          map("n", "<leader>hp", gitsigns.preview_hunk)
+          map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+
+          map("n", "<leader>hb", function()
+            gitsigns.blame_line({ full = true })
+          end)
+
+          map("n", "<leader>hd", gitsigns.diffthis)
+
+          map("n", "<leader>hD", function()
+            gitsigns.diffthus("~")
+          end)
+
+          map("n", "<leader>hQ", function() gitsigns.setqflist("all") end)
+          map("n", "<leader>hq", gitsigns.setqflist)
+
+          -- Toggles
+          map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+          map("n", "<leader>tw", gitsigns.toggle_word_diff)
+
+          -- Text objects
+          map({"o", "x"}, "ih", gitsigns.select_hunk)
+        end
+      },
+    },
+    { "ntpeters/vim-better-whitespace" },
   },
   install = { colorscheme = { "modus" } },
   checker = { enabled = true },
@@ -68,8 +145,10 @@ vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 
 -- Visual mode
 
--- Theme
-vim.cmd([[colorscheme modus]])
-
 -- Setup
 vim.opt.nu = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+vim.o.colorcolumn = "80"
