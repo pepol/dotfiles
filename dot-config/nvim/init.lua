2fcd4970de392e341c1ac2db9e44e43653833336
Peter Polacik <peter@polacik.org>
2025-06-04 12:41:40 +0200

    neovim/ts: automatically install bindings for some languages

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -140,7 +140,18 @@ require("lazy").setup({
 vim.lsp.enable("gopls")
 
 -- TS
-require'nvim-treesitter.configs'.setup({
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",
+    "bash", "cmake", "commonlisp", "css", "cue", "diff", "dockerfile",
+    "editorconfig", "elixir", "erlang", "git_config", "git_rebase",
+    "gitattributes", "gitcommit", "gitignore", "go", "gomod", "gosum",
+    "gotmpl", "gowork", "groovy", "helm", "html", "http", "ini", "java",
+    "javadoc", "javascript", "jq", "json", "jsonnet", "llvm",
+    "make", "nginx", "objdump", "passwd", "pem", "perl", "promql", "proto",
+    "python", "regex", "rust", "sql", "ssh_config", "strace", "terraform",
+    "tmux", "toml", "typescript", "yaml",
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
